ddb2770146f437bf63c88152bf7674e273a13085
Vyacheslav <vbruk@hey.com>
2021-05-03 16:22:47 -0300

    Add static pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class PagesController < ApplicationController
+  def index
+  end
+
+  def selects
+  end
+
+  def h_forms
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+
+  get '/pages/selects', to: 'pages#selects', as: :pages_selects
+  get '/pages/h_forms', to: 'pages#h_forms', as: :pages_h_forms
 end
